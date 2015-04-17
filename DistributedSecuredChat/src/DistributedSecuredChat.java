2a724466657e7516ddca94375dce22dcb7a02e2a
shubham <shubhammehta.93@gmail.com>
2015-04-17 20:22:39 +0530

    Revert "aes key again again again"
    This reverts commit bdb801778e3e27b447d14c5519111949ed89461f.

--- DistributedSecuredChat/src/DistributedSecuredChat.java
+++ DistributedSecuredChat/src/DistributedSecuredChat.java
@@ -19,8 +19,6 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
 public class DistributedSecuredChat {
 	static User me;
 	static RmiServer rmi_obj = null;
@@ -167,18 +165,18 @@ public class DistributedSecuredChat {
 	}
 
 	public static String encryptMessage(String inp, SecretKeySpec spec) throws Exception {
-		byte[] inpBytes = Base64.decode(inp);
+		byte[] inpBytes = inp.getBytes("UTF8");
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.ENCRYPT_MODE, spec);
 		byte[] cipherData = cipher.doFinal(inpBytes);
-		return Base64.encode(cipherData);
+		return new String(cipherData,"UTF8");
 	}
 	
 	public static String decryptMessage(String inp, SecretKeySpec spec) throws Exception {
-		byte[] inpBytes = Base64.decode(inp);
+		byte[] inpBytes = inp.getBytes("UTF8");
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.ENCRYPT_MODE, spec);
 		byte[] cipherData = cipher.doFinal(inpBytes);
-		return Base64.encode(cipherData);
+		return new String(cipherData,"UTF8");
 	}
 }
